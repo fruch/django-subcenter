0b7ec135c7d28bf2fdd99353beb381a4762e6eb4
Israel Fruchter <israel.fruchter@gmail.com>
2010-09-27 00:25:40 +0200

    a type again

--- sub/test.bat
+++ sub/test.bat
@@ -1,1 +1,1 @@
-python manage.py test movies shows persons utils --with-coverage --cover-package=movies,shows,persons,utils --with-xunit --with-xcoverage --pdb
+python manage.py test movies shows persons utils --with-coverage --cover-package=movies,shows,persons,utils --with-xunit --with-xcoverage
