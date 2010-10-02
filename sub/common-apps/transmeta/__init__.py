27bad7daf1adca38a3cf036a0db43e7c3f4110c2
Israel Fruchter <israel.fruchter@gmail.com>
2010-10-02 20:57:39 +0200

    fix transmeta field names to be translatable

--- sub/common-apps/transmeta/__init__.py
+++ sub/common-apps/transmeta/__init__.py
@@ -129,8 +129,8 @@ class TransMeta(models.base.ModelBase):
                         lang_attr.null = True
                     if not lang_attr.blank:
                         lang_attr.blank = True
-                if lang_attr.verbose_name:
-                    lang_attr.verbose_name = u'%s %s' % (lang_attr.verbose_name, lang_code)
+                #if lang_attr.verbose_name:
+                #    lang_attr.verbose_name = u'%s %s' % (lang_attr.verbose_name, lang_code)
                 attrs[lang_attr_name] = lang_attr
             del attrs[field]
             attrs[field] = property(default_value(field))
